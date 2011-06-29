438d7f7d23079538d767c5d7a23c24f826c26014
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-06-29 18:43:39 +0900

    fix retry

--- lib/pushr/server.rb
+++ lib/pushr/server.rb
@@ -1,5 +1,3 @@
-require 'error_info'
-
 require 'publisher/gmail'
 require 'subscriber/boxcar'
 
@@ -10,24 +8,25 @@ module Pushr
     end
 
     def run
-      @logger.info 'Run'
+      begin
+        @logger.info 'Run'
 
-      @publisher  = Pushr::Publisher::Gmail.new $config
-      @subscriber = Pushr::Subscriber::Boxcar.new $config
+        @publisher  = Pushr::Publisher::Gmail.new $config
+        @subscriber = Pushr::Subscriber::Boxcar.new $config
 
-      loop do
-        @publisher.start { |info| @subscriber.notify info }
-        sleep 30
-      end
+        loop do
+          @publisher.start { |info| @subscriber.notify info }
+          sleep 30
+        end
 
-    rescue Exception => e
-      @logger.error e
-      @subscriber.notify "#{Pushr::ErrorInfo.new e}. Retry after 300 seconds"
-      sleep 300
-      retry
-    ensure
-      @publisher.destruct
-      @subscriber.destruct
+      rescue Exception => e
+        @logger.error e
+        @logger.error e 'Retry after 300 seconds'
+        sleep 300
+        retry
+      ensure
+        @publisher.destruct
+        @subscriber.destruct
     end
 
     def self.run
